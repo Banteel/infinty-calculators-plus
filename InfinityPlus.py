2c25b121fad7145599f8c4adb35f9e173d0c79ab
Banteel <68601263+Banteel@users.noreply.github.com>
2026-02-10 17:37:47 -0500

    Update InfinityPlus.py
    * repeatedly choose focus on any permutation and change the number size of displayed decimals.
    * redisplay original calculations
    * choose new Work Numbers without closing the script
    * includes as option "q" = Quit on most input requests

--- InfinityPlus.py
+++ InfinityPlus.py
@@ -2,13 +2,13 @@
 # InfintyPlus
 # based on Copyright (c)
 
-#	Copyright (c) 2025 (InfintyNow), Copyright (c) 2027 (InfintyPlus) Trevor McQuaker
+#        Copyright (c) 2025 (InfintyNow), Copyright (c) 2027 (InfintyPlus) Trevor McQuaker
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted under the terms of the Apache-2.0
 
-#	Copyright 2025, 2027 Trevor McQuaker
+#        Copyright 2025, 2027 Trevor McQuaker
 
 #Licensed under the Apache License, Version 2.0 (the "Apache-2.0");
 #you may not use this file except in compliance with the License.
@@ -23,78 +23,178 @@
 #limitations under the License.
 
 
-# 	A Sub-calculator (named):
-#	"InfintyPlus""
-#	The sub-calculator located here in the Python file is the results of sub-calculations permutated from two seporate equations performing the exact same results, for example here is always consistently the number "one".
+#         A Sub-calculator (named):
+#        "InfintyPlus""
+#        The sub-calculator located here in the Python file is the results of sub-calculations permutated from two seporate equations performing the exact same results, for example here is always consistently the number "one".
 
 import os
 #import numpy as np
 import math
-from math import ceil
-from decimal import Decimal, localcontext, InvalidOperation
+import sys
+from decimal import Decimal, localcontext, Context, InvalidOperation
 
 def InfinityPlus():
-	print('		Number Sub-Calculator: "InfinityPlus"')
+	num1 = None
+	num2 = None
+	num3 = None
+	focus = None
+	yesorno = None
+	
+	print('          Number Sub-Calculator: "InfinityPlus"')
 	
 	with localcontext() as ctx:
-		ctx.prec = 1000000
+		ctx.prec = 100000000
+		
 		while True:
+			if num1 == 'q' or num2 == 'q' or num3 == 'q' or focus == 'q' or yesorno == 'q':
+				break
+			yesorno = None
 			try:
-				print('   "Input Whole Numbers (21), Fractions (.21), Whole number with fraction (21.21), or Whole number with fraction including "exponent" eg. "21.21e21" or "21.21e-21"')
-				num1 = input('   \n   Type "quit" or Enter value #1:\n').lower()
-				if num1 == "quit":
-					break
-					
-				num2 = input('   \n   Type "quit" or Enter value #2:\n').lower()
-				if num2 == "quit":
-					break
+				print('    Input Whole Numbers (21), Fractions (.21), Whole number with fraction (21.21), or Whole number with fraction including "exponent" eg. "21.21e21" or "21.21e-21')
 				
+				num1 = input('   \n   Input "q" to Quit or input value for #1:\n').lower()
 				a = Decimal(num1)
-				b = Decimal(num2)
-				
 			except (ValueError, InvalidOperation):
-				print('   Error: Please enter valid numbers in scientific or decimal format. (ex. 21, 21.21, .21, 21.21e21 or enter  "quit" to close.')
-				continue
-				
-			p1 = a / b
-			p2 = a - b
-			eq1 = p1 - (p2 / b)
-			print(f'\n   Calibrator Macro: \n   a ÷ b - (a - b) ÷ b\n   = {eq1: .2g}')
-			result1 = a / b
-			print(f'   A: a ÷ b\n   ={result1.normalize(): .120g}')
-			result2 = (a / b) - (a - b)
-			print(f"   B: a ÷ b - (a - b)\n   ={result2: .120g}")
-			result3 = b - (a - b) / b
-			print(f"   C: b - (a - b) ÷ b\n   ={result3: .120g}")
-			result4 = b - (a - b)
-			print(f"   D: b - (a - b)\n   ={result4: .120g}")
-			result5 = a - b
-			print(f"   E: (a - b)\n   ={result5: .120g}")
-			result6 = (a - b) / b
-			print(f"   F: (a - b) ÷ b\n   ={result6: .120g}")
-				
-			eq2 = (((a / b) / a) * b)
-			print(f"\n   Calibrator Micro:\n   a ÷ b ÷ a × b\n   = {eq2: .2g}")
-			result7 = a / b
-			print(f"   H:  a ÷ b\n   ={result7: .120g}")
-			result8 = (a / b) / a
-			print(f"   I: a ÷ b ÷ a\n   ={result8: .120g}")
-			result9 = (b / a) * b
-			print(f"   J:  b ÷ a × b\n   ={result9: .120g}")
-			result10 = b / a
-			print(f"   K: b ÷ a\n   ={result10: .120g}")
-			result11 = a * b
-			print(f"   L: a × b\n   ={result11: .120g}")
-			input("\n   Program paused.\n   Press the key (Enter or Return) to Reset Program and input new numbers.\n\n")
-			
-		if os.name == 'nt':
-			os.system('cls')
-		else:
-			os.system('clear')
-	print('\n   "InfintyPlus" script closed.\n')
-			
+				if num1 == 'q':
+					quit()
+				else:
+					print('   Error: Please input a valid number in scientific or decimal format. (ex. 21, 21.21, .21, 21.21e21 or input "q" to quit.')
+					continue
+			finally:
+				pass
+
+			try:
+				num2 = input('   \n   Input "q" to Quit or input value for #2:\n').lower()
+				b = Decimal(num2)
+			except (ValueError, InvalidOperation):
+				if num2 == 'q':
+					quit()
+				else:
+					print('   Error: Please input a valid number in scientific or decimal format. (ex. 21, 21.21, .21, 21.21e21 or input "q" to quit.')
+					continue
+			finally:
+				pass
+
+			try:
+				num3 = input('   \n   Input "q" to Quit or input decimal length value:\n').lower()
+				c = Decimal(num3)
+				tempc = c
+			except (ValueError, InvalidOperation):
+				if num3 == 'q':
+					quit()
+				else:
+					print('   Error: Please input a valid number in scientific or decimal format. (ex. 21, 21.21, .21, 21.21e21 or input "q" to quit.')
+					continue
+			finally:
+				pass
+
+			while True:
+				if num1 == 'q' or num2 == 'q' or num3 == 'q' or focus == 'q' or yesorno == 'q' or yesorno == 'n':
+					break
+				try:
+					if focus == None or yesorno == 'n' or (focus in 'abcdef' and yesorno == 'y'):
+						eq1 = (a / b) - ((a - b) / b)
+						print(f'\n   Macro Calibrator:\n   a ÷ b - (a - b) ÷ b\n   =   {eq1: .2g}')
+					elif focus in 'abcdefghijkl':
+						print(f'\n   You selected: {focus.upper()}')
+					else:
+						print('   Invalid input. Please input a letter from (a-k) or input "q" to Quit.')
+						continue
+	
+					if focus == 'a' or focus == None:
+						result1 = a / b
+						print(f'\n   A: a ÷ b\n   =   {result1: .{c}g}')
+	
+					if focus == 'b' or focus == None:
+						result2 = (a / b) - (a - b)
+						print(f"\n   B: a ÷ b - (a - b)\n   =   {result2: .{c}g}")
+	
+					if focus == 'c' or focus == None:
+						result3 = b - (a - b) / b
+						print(f"\n   C: b - (a - b) ÷ b\n   =   {result3: .{c}g}")
+	
+					if focus == 'd' or focus == None:
+						result4 = b - (a - b)
+						print(f"\n   D: b - (a - b)\n   =   {result4: .{c}g}")
+	
+					if focus == 'e' or focus == None:
+						result5 = a - b
+						print(f"\n   E: (a - b)\n   =   {result5: .{c}g}")
+	
+					if focus == 'f' or focus == None:
+						result6 = (a - b) / b
+						print(f"\n   F: (a - b) ÷ b\n   =   {result6: .{c}g}")
+	
+					if focus == None or yesorno == 'n' or (focus in 'ghijk' and yesorno == 'y'):
+						eq2 = (((a / b) / a) * b)
+						print(f"\n   Micro Calibrator:\n   a ÷ b ÷ a × b\n   =   {eq2: .2g}")
+	
+					if focus == 'g' or focus == None:
+						result7 = a / b
+						print(f"\n   G:  a ÷ b\n   =   {result7: .{c}g}")
+	
+					if focus == 'h' or focus == None:
+						result8 = (a / b) / a
+						print(f"\n   H: a ÷ b ÷ a\n   =   {result8: .{c}g}")
+	
+					if focus == 'i' or focus == None:
+						result9 = (b / a) * b
+						print(f"\n   I:  b ÷ a × b\n   =   {result9: .{c}g}")
+	
+					if focus == 'j' or focus == None:
+						result10 = b / a
+						print(f"\n   J: b ÷ a\n   =   {result10: .{c}g}")
+	
+					if focus == 'k' or focus == None:
+						result11 = a * b
+						print(f"\n   K: a × b\n   =   {result11: .{c}g}")
+				finally:
+					pass
+#				except (ValueError, InvalidOperation):
+#					print('\n   Invalid input. Please input a letter from (a-k) or input "q" to Quit.')
+	
+				while True:
+					if num1 == 'q' or num2 == 'q' or num3 == 'q' or focus == 'q' or yesorno == 'q' or yesorno == 'n':
+						break
+					try:
+						yesorno = input('\n   Program paused.\n   Change the decimal amount for an output from (a to k)?\n   (y) = Choose a permutation and a decimal amount. \n   (n) = Choose new Work Numbers for Sub-calculations.\n   (d) = Display previous Sub-calculations again  \n   (q) = Quit.\n').lower()
+		
+						if yesorno == 'q':
+							break
+						elif yesorno == 'y':
+							focus = input('\n   Choose a Permutation letter output from (a to k):\n').lower()
+							num3 = input('\n   Program paused.\n   Input a number to change it\'s amount of decimals:\n').lower()
+							c = num3
+							break
+						elif yesorno == 'd':
+							focus = None
+							c = tempc
+							break
+						elif yesorno == 'n':
+							focus = None
+							num1 = None
+							num2 = None
+							num3 = None
+							break
+						else:
+								print('\n   Invalid input. Choose input "y" or "n" or "d" or "q".')
+					except (ValueError, InvalidOperation):
+						if yesorno == 'q':
+							break
+						else:
+							continue
+					finally:
+						pass
+
+#		if os.name == 'nt':
+#			os.system('cls')
+#		else:
+#			os.system('clear')
+					
+		print('\n   "InfintyPlus" script closed.\n')
+
 if __name__ == "__main__":
-	InfinityPlus()
+        InfinityPlus()
 
 #def format_repeating(n, d):
 #    res = [str(n // d), "."]
